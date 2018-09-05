9707ca4bc74fa3789c3f30e74d5f71c906a1bb0f
Ernest Guok <enenyi@gmail.com>
2018-09-05 17:42:56 +0800

    Debug effort 12

--- js/experiment.js
+++ js/experiment.js
@@ -51,7 +51,7 @@ function initExperiment() {
 	var records = data.split("\n");
 	numTrials = records.length - 1;
 	for (var i = 1; i <= numTrials; i++) {
-		var cells = records[i];
+		var cells = records[i].split(",");
 		var menuType = cells[0].trim();
 		var menuDepth = cells[1].trim();
 		var menuBreadth = cells[2].trim();
